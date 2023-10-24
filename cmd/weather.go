533894c1adc2ffcb8ad4d845ffdbf930421d81b9
下平大輝 <shimodairadaiki@shimodairadaiakiranoMacBook-Air.local>
2023-10-24 19:43:04 +0900

    add select cityName

--- cmd/weather.go
+++ cmd/weather.go
@@ -4,6 +4,10 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"bufio"
+	"fmt"
+	"os"
+
 	"github.com/Shimodaira0910/weather/api"
 	"github.com/spf13/cobra"
 )
@@ -12,8 +16,13 @@ import (
 var weatherCmd = &cobra.Command{
 	Use:   "weather",
 	Run: func(cmd *cobra.Command, args []string) {
+		scanner := bufio.NewScanner(os.Stdin)
+		fmt.Println("検索したい都市名を、ローマ字で入力してください(文字の最初は大文字です)")
+		scanner.Scan()
+		cityName := scanner.Text()
+		
 		api := api.Weather{}
-		api.GetWeatherInfo("Osaka")
+		api.GetWeatherInfo(cityName)
 	},
 }
 
