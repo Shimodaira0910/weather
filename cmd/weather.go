4a0ba5091bda56c38f2709425836567806e5fd8e
下平大輝 <shimodairadaiki@shimodairadaiakiranoMacBook-Air.local>
2023-10-30 20:55:21 +0900

    add error message

--- cmd/weather.go
+++ cmd/weather.go
@@ -17,9 +17,20 @@ var weatherCmd = &cobra.Command{
 	Use:   "weather",
 	Run: func(cmd *cobra.Command, args []string) {
 		scanner := bufio.NewScanner(os.Stdin)
-		fmt.Println("検索したい都市名を、ローマ字で入力してください(文字の最初は大文字です)")
-		scanner.Scan()
-		cityName := scanner.Text()
+		cityName := ""
+		flag := 0
+		
+		for flag == 0{
+			fmt.Println("検索したい都市名を、ローマ字で入力してください(文字の最初は大文字です)")
+			scanner.Scan()
+			cityName := scanner.Text()
+
+			if cityName == ""{
+				fmt.Println("都市名を入力してください。")
+			} else if cityName != ""{
+				flag += 1
+			} 
+		}
 		
 		api := api.Weather{}
 		api.GetWeatherInfo(cityName)
